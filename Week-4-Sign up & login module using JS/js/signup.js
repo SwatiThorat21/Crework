590adb0d20d9f8d3ffd3de748d712966d731fa20
Swati <swatic019@gmail.com>
2022-08-12 08:29:16 +0530

    email validation using regex

--- Week-4-Sign up & login module using JS/js/signup.js
+++ Week-4-Sign up & login module using JS/js/signup.js
@@ -1,11 +1,18 @@
+function emailValidate(e) {
+  let inputText = document.getElementById("email").value;
 
-function emailValidate(){
-    let inputText = document.getElementById('email').value;
-   
-    if(inputText.indexOf('@')<1){
-        alert('Please add @');
-        return false;
-    }
-   
+  // if(inputText.indexOf('@')<1){
+  //     alert('Please add @');
+  //     return false;
+  // }
+
+  let mailFormat = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
+
+  if (!inputText.match(mailFormat)) {
+    document.getElementById("errorMsg").style.display = "block";
+  }
+  e.preventDefault();
+  return false;
+    
 }
 
