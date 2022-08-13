9aee9ca66c34b348ccff893bb36c1d773ae391ff
Swati <swatic019@gmail.com>
2022-08-14 00:26:28 +0530

    Signup module updated

--- Week-4-Sign-up-login-module-using-JS/js/signup.js
+++ Week-4-Sign-up-login-module-using-JS/js/signup.js
@@ -1,17 +1,34 @@
 function formValidate() {
+  //Name validation
+  let inputName = document.getElementById("name").value;
+  if(inputName == "" || inputName == null){
+    document.getElementById("name-error").style.display = "block";
+    document.getElementById("name-error").classList.add('error-msg');
+    document.getElementById("name").classList.add('input');
+  }
   
-  let inputText = document.getElementById("email").value;
-
+  //Email Validation
+  let inputMail = document.getElementById("email").value;
   let mailFormat = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 
-  if (!inputText.match(mailFormat)) {
-    document.getElementById("errorMsg").style.display = "block";
-    return false;
+  if (!inputMail.match(mailFormat)) {
+    document.getElementById("mail-error").style.display = "block";
+    document.getElementById("mail-error").classList.add('error-msg');
+    document.getElementById("email").classList.add('input');
   }
-  else{
-    document.getElementById("errorMsg").style.display = "none";
-    return true;
-   }
-      
-}
 
+  //Password validation
+  let inputPassword = document.getElementById("password").value;
+  let numbers = /[0-9]/g;
+
+  if (!inputPassword.match(numbers)) {
+    document.getElementById("pass-error1").style.display = "block";
+    document.getElementById("pass-error1").classList.add('error-msg');
+    document.getElementById("password").classList.add('input');
+  }
+  else if (inputPassword.length < 4 || inputPassword.length > 12) {
+    document.getElementById("pass-error2").style.display = "block";
+    document.getElementById("pass-error2").classList.add('error-msg');
+    document.getElementById("password").classList.add('input');
+  }
+}
