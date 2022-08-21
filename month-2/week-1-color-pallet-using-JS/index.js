7c87eee457f680f0c7125c0bef50b8cff79d2660
Swati <swatic019@gmail.com>
2022-08-21 12:41:35 +0530

    color pallet JS added

--- month-2/week-1-color-pallet-using-JS/index.js
+++ month-2/week-1-color-pallet-using-JS/index.js
@@ -1,10 +1,11 @@
 let button = document.querySelector('button');
 let h2elem = document.querySelector('h2');
-let background = document.getElementById('container-1');
+let bgColor = document.getElementsByClassName('container-1')[0];
 
 button.addEventListener('click',()=>{
     let color = '#';
     color += Math.random().toString(16).slice(2,8);
-    console.log(color);
-    background.style.backgroundColor = color;
+    
+    bgColor.style.backgroundColor = color;
+    h2elem.innerHTML = color;
 })
