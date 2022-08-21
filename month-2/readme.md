1d4e81c0cd6f30bb7affef2f19f8ea134e74f714
Swati <swatic019@gmail.com>
2022-08-21 12:51:54 +0530

    readme added

---
+++ month-2/readme.md
@@ -0,0 +1,4 @@
+<h1>Color Pallet Generator USing Javascript</h1>
+<p>In the First week of second month we have to create color pallet generator using Javascript</p>
+<p>Below is the output of code</p>
+<img src= "images\color pallet.JPG">
--- month-2/week-1-color-pallet-using-JS/index.js
+++ month-2/week-1-color-pallet-using-JS/index.js
@@ -1,11 +1,11 @@
-let button = document.querySelector('button');
-let h2elem = document.querySelector('h2');
-let bgColor = document.getElementsByClassName('container-1')[0];
+let button = document.querySelector("button");
+let h2elem = document.querySelector("h2");
+let bgColor = document.getElementsByClassName("container-1")[0];
 
-button.addEventListener('click',()=>{
-    let color = '#';
-    color += Math.random().toString(16).slice(2,8);
-    
-    bgColor.style.backgroundColor = color;
-    h2elem.innerHTML = color;
-})
+button.addEventListener("click", () => {
+  let color = "#";
+  color += Math.random().toString(16).slice(2, 8);
+
+  bgColor.style.backgroundColor = color;
+  h2elem.innerHTML = color;
+});
