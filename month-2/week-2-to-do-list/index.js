6649cf91261bc194aca25a410d5a87d5acd2780c
Swati <swatic019@gmail.com>
2022-08-28 17:02:12 +0530

    function showlist updated

--- month-2/week-2-to-do-list/index.js
+++ month-2/week-2-to-do-list/index.js
@@ -1,27 +1,43 @@
-let addBtn = document.querySelector('.add-btn');
-
-addBtn.addEventListener('click', (element)=>{
-let addInput = document.getElementById('addInput').value;
-let listObj= [];
-listObj.push(addInput);
-localStorage.setItem('list', JSON.stringify(listObj));
 showList();
+
+let addBtn = document.querySelector(".add-btn");
+
+addBtn.addEventListener("click", (element) => {
+  let addInput = document.getElementById("addInput").value;
+  let list = localStorage.getItem('list');
+  if(list == null){
+    listObj = [];
+  }
+  else{
+    listObj = JSON.parse(list);
+  }
+  listObj.push(addInput);
+  localStorage.setItem("list", JSON.stringify(listObj));
+  addInput = "";
+  showList();
 });
 
-function showList(){
-let list = localStorage.getItem('list');
-listObj = JSON.parse(list);
-let html = "" ;
-listObj.forEach(element => {
+function showList() {
+  let list = localStorage.getItem("list");
+  if(list == null){
+    listObj = [];
+  }
+  else{
+  listObj = JSON.parse(list);
+  }
+
+  let html = "";
+  listObj.forEach((element) => {
     html += `
-                <div class="li-items"> 
-                <li> ${element}</li>
-                <div id="i-tags">
-                  <i class="fa-regular fa-pen-clip"></i>
-                  <i class="fa-regular fa-trash-can"></i>
-                </div>
-       `
-});
-
-
+    <ul>            
+    <div class="li-items"> 
+    <li> ${element}</li>
+    <div id="i-tags">
+    <i class="fa-regular fa-pen-clip"></i>
+    <i class="fa-regular fa-trash-can"></i>
+    </div>
+    </ul> `;
+  });
+  let listItems = document.getElementById('listItems');
+  listItems.innerHTML = html;
 }
