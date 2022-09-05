3e0d8b48acdd7cd88d68d95bcfd13eda0ca474d0
Swati <swatic019@gmail.com>
2022-09-05 23:42:28 +0530

    API key created

---
+++ month-2/week-3-movie-app-API/code.js
@@ -0,0 +1,3 @@
+let API_Key = '34a049d3fb8f5b726b30b0eaca49c7db';
+let BASE_URL = 'https://api.themoviedb.org/3';
+let API_URL = BASE_URL + '/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc' + API_Key;
