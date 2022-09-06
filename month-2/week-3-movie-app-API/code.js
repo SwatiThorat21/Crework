9ca1169ad091023781732b65f105004fdf43d4cd
Swati <swatic019@gmail.com>
2022-09-07 00:02:09 +0530

    search function added

--- month-2/week-3-movie-app-API/code.js
+++ month-2/week-3-movie-app-API/code.js
@@ -1,13 +1,16 @@
+let API_Key = '34a049d3fb8f5b726b30b0eaca49c7db';
 let API_URL = 'https://api.themoviedb.org/3/tv/popular?api_key=34a049d3fb8f5b726b30b0eaca49c7db&language=en-US&page=1'
 
 let IMG_URL = 'https://image.tmdb.org/t/p/w500';
-
+let search_URL = 'https://api.themoviedb.org/3/search/movie?api_key=34a049d3fb8f5b726b30b0eaca49c7db&query=Jack+Reacher';
 let main = document.getElementById('main');
+let form = document.getElementById('form');
+let search = document.getElementById('search');
 
 getMovies(API_URL);
 function getMovies(url){
     fetch(url).then(res => res.json()).then(data => {
-        console.log(data.results);
+        // console.log(data.results);
         showMovies(data.results);
     })
 }
@@ -45,3 +48,11 @@ function getColor(vote){
         return 'red'
     }
 }
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    let searchTerm = search.value;
+    if(searchTerm){
+        getMovies(search_URL+'&query='+searchTerm);
+    }
+})
