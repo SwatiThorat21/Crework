5e0f6505e0ebadf3be368d793b82a5702a455a4e
Swati <swatic019@gmail.com>
2022-09-11 17:18:09 +0530

    JS and readme added

---
+++ month-2/week-4-calculator-using-OOP/code.js
@@ -0,0 +1,25 @@
+
+class Calculator {
+constructor(number1, number2){
+    this.number1 = number1;
+    this.number2 = number2;
+}
+
+ add(number1, number2){
+  return number1 + number2;
+}
+
+minus(number1, number2){
+    return number1 - number2;
+  }
+
+multiply(number1, number2){
+    return number1 * number2;
+  }
+
+division(number1, number2){
+    return number1 / number2;
+  }  
+}
+
+
---
+++ month-2/week-4-calculator-using-OOP/readme.md
@@ -0,0 +1,4 @@
+<h1>Calculator Using Javascript OOPs</h1>
+<p>In the 4th week of 2nd month we have given task of creating Calculator using Javascript Object Oriented Programming</p>
+<p>This is the result of the calculator in console</p>
+<img src="images/calculator.JPG>
